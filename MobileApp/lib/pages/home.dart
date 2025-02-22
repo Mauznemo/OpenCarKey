d74e738edb8e9ec03b9c9f04ec8a3423188fcba6
Prezes2137 <michal.heb.pr@gmail.com>
2025-02-23 00:52:35 +0100

    Rebuild home page

--- MobileApp/lib/pages/home.dart
+++ MobileApp/lib/pages/home.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:open_car_key_app/services/ble_event_listener.dart';
-import 'package:open_car_key_app/services/ble_service.dart';
-import 'package:open_car_key_app/services/vehicle.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../components/add_vehicle_bottom_sheet.dart';
 import '../components/edit_vehicle_bottom_sheet.dart';
+import '../services/ble_service.dart';
+import '../services/vehicle_service.dart';
+import '../types/vehicle.dart';
+import '../utils/utils.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,13 +19,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-
-  List<VehicleEntry> _vehicleEntries = [];
+  List<Vehicle> vehicles = [];
 
   late String _event = "--";
 
-  final List<String> _notAuthenticatedDevices = [];
+  final List<String> notAuthenticatedDevices = [];
 
   Future<void> _requestPermission() async {
     Map<Permission, PermissionStatus> statuses = await [
@@ -33,95 +34,119 @@ class _HomePageState extends State<HomePage> {
     ].request();
   }
 
-  void _getVehicles() async {
-    var vehicles = await VehicleStorage.getVehicles();
-    _vehicleEntries = vehicles
-        .map((vehicle) => VehicleEntry(
-            vehicleData: vehicle, isConnected: false, doorsLocked: false))
-        .toList();
+  void getVehicles() async {
+    final vehiclesData = await VehicleStorage.getVehicles();
 
-    _getConnectedDevices();
+    setState(() {
+      vehicles = vehiclesData
+          .map((vehicle) => Vehicle(
+                device: BluetoothDevice.fromId(vehicle.macAddress),
+                data: vehicle,
+              ))
+          .toList();
+    });
+
+    connectDevices();
+
+    // getConnectedDevices();
+  }
+
+  void connectDevices() async {
+    for (final vehicle in vehicles) {
+      await vehicle.device.connect();
+    }
+  }
+
+  void getConnectedDevices() async {
+    final connectedDevices = await BleService.getConnectedDevices();
+
+    for (final vehicle in vehicles) {
+      final connectedDevice = connectedDevices.firstWhere(
+        (device) => device.remoteId == vehicle.device.remoteId,
+        orElse: () => vehicle.device,
+      );
+
+      vehicle.device = connectedDevice;
+      vehicle.device.connect();
+    }
+
+    if (connectedDevices.isNotEmpty)
+      await BleService.sendMessage(connectedDevices.first, "ds");
+
     setState(() {});
   }
 
-  void _getConnectedDevices() async {
-    var connectedDevices = await BleService.getConnectedDevices();
-
-    for (var macAddress in connectedDevices) {
-      _vehicleEntries
+  void processMessage(String macAddress, String message) {
+    if (message.startsWith("NOT_AUTH") || message.startsWith("AUTH_FAIL")) {
+      if (notAuthenticatedDevices.contains(macAddress)) {
+        return;
+      }
+      notAuthenticatedDevices.add(macAddress);
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text("Not Authenticated"),
+                content: Text(
+                    "The device $macAddress you are trying to communicate with is not authenticated. Please make sure the pin is correct."),
+                actions: [
+                  TextButton(
+                      onPressed: Navigator.of(context).pop,
+                      child: const Text("OK"))
+                ],
+              ));
+    } else if (message.startsWith("AUTH_OK")) {
+      if (notAuthenticatedDevices.contains(macAddress)) {
+        notAuthenticatedDevices.remove(macAddress);
+      }
+    } else if (message.startsWith("ld")) {
+      setState(() => vehicles
           .firstWhere((element) =>
-              element.vehicleData.macAddress.toLowerCase() ==
-              macAddress.toLowerCase())
-          .isConnected = true;
+              element.data.macAddress.toLowerCase() == macAddress.toLowerCase())
+          .doorsLocked = true);
+    } else if (message.startsWith("ud")) {
+      setState(() => vehicles
+          .firstWhere((element) =>
+              element.data.macAddress.toLowerCase() == macAddress.toLowerCase())
+          .doorsLocked = false);
+    } else if (message.startsWith("ut")) {
+      scaffoldMessengerKey.currentState?.showSnackBar(
+        const SnackBar(
+          content: Text("Trunk unlocked"),
+        ),
+      );
     }
-
-    BleService.postEvent("SEND_MESSAGE:ds\n");
-
-    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
-    _getVehicles();
-    _getConnectedDevices();
+    getVehicles();
 
-    BleEventListener.listenForBleEvents((event) {});
+    FlutterBluePlus.events.onConnectionStateChanged.listen((event) async {
+      Vehicle changedVehicle = vehicles.firstWhere(
+          (vehicle) => vehicle.device.remoteId == event.device.remoteId);
 
-    BleEventListener.onDeviceConnected = (macAddress) {
-      _event = "Connected to $macAddress";
-      _vehicleEntries
-          .firstWhere((element) =>
-              element.vehicleData.macAddress.toLowerCase() ==
-              macAddress.toLowerCase())
-          .isConnected = true;
+      changedVehicle.device = event.device;
+
+      if (event.connectionState == BluetoothConnectionState.connected)
+        await BleService.sendMessage(
+            changedVehicle.device, "AUTH:${changedVehicle.data.pin}");
+
+      setState(() =>
+          vehicles.firstWhere(
+              (vehicle) => vehicle.device.remoteId == event.device.remoteId) ==
+          changedVehicle);
+    });
+
+    FlutterBluePlus.events.onCharacteristicReceived.listen((event) {
+      _event = "Characteristic received: ${utf8.decode(event.value)}";
+      print("Characteristic received: ${utf8.decode(event.value)}");
+
+      processMessage(
+          event.device.remoteId.toString(), utf8.decode(event.value));
+
       setState(() {});
-    };
-
-    BleEventListener.onDeviceDisconnected = (macAddress) {
-      _event = "Disconnected from $macAddress";
-      _vehicleEntries
-          .firstWhere((element) =>
-              element.vehicleData.macAddress.toLowerCase() ==
-              macAddress.toLowerCase())
-          .isConnected = false;
-      setState(() {});
-    };
-
-    BleEventListener.onMessageReceived = (macAddress, message) {
-      if (message.startsWith("NOT_AUTH")) {
-        if (_notAuthenticatedDevices.contains(macAddress)) {
-          return;
-        }
-        _notAuthenticatedDevices.add(macAddress);
-        showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-                  title: const Text("Not Authenticated"),
-                  content: Text(
-                      "The device $macAddress you are trying to communicate with is not Authenticated. Please make sure the Pin is correct."),
-                  actions: [
-                    TextButton(
-                        onPressed: Navigator.of(context).pop,
-                        child: const Text("OK"))
-                  ],
-                ));
-      } else if (message.startsWith("ld")) {
-        _vehicleEntries
-            .firstWhere((element) =>
-                element.vehicleData.macAddress.toLowerCase() ==
-                macAddress.toLowerCase())
-            .doorsLocked = true;
-        setState(() {});
-      } else if (message.startsWith("ud")) {
-        _vehicleEntries
-            .firstWhere((element) =>
-                element.vehicleData.macAddress.toLowerCase() ==
-                macAddress.toLowerCase())
-            .doorsLocked = false;
-        setState(() {});
-      }
-    };
+    });
   }
 
   @override
@@ -129,6 +154,15 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Open Car Key"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                getVehicles();
+                setState(() {});
+              },
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
@@ -140,7 +174,7 @@ class _HomePageState extends State<HomePage> {
               _event =
                   prefs.getString('device_mac_address') ?? 'Not associated';
               setState(() {});*/
-            _getVehicles();
+            getVehicles();
           },
           child: const Icon(Icons.add),
         ),
@@ -149,9 +183,9 @@ class _HomePageState extends State<HomePage> {
           children: [
             ListView.builder(
                 shrinkWrap: true,
-                itemCount: _vehicleEntries.length,
+                itemCount: vehicles.length,
                 itemBuilder: (context, index) {
-                  VehicleEntry vehicleEntry = _vehicleEntries[index];
+                  Vehicle vehicle = vehicles[index];
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Builder(builder: (context) {
@@ -159,8 +193,8 @@ class _HomePageState extends State<HomePage> {
                       return ListTile(
                         onLongPress: () async {
                           await EditVehicleBottomSheet.showBottomSheet(
-                              context, vehicleEntry.vehicleData);
-                          _getVehicles();
+                              context, vehicle);
+                          getVehicles();
                           setState(() {});
                         },
                         shape: RoundedRectangleBorder(
@@ -173,58 +207,67 @@ class _HomePageState extends State<HomePage> {
                             Row(
                               children: [
                                 Icon(
-                                  vehicleEntry.isConnected
+                                  vehicle.device.isConnected
                                       ? Icons.bluetooth_audio
                                       : Icons.bluetooth_disabled_rounded,
-                                  color: vehicleEntry.isConnected
+                                  color: vehicle.device.isConnected
                                       ? Colors.green
                                       : Colors.red,
                                 ),
                                 SizedBox(width: 10),
-                                Text(vehicleEntry.vehicleData.name,
-                                    style: const TextStyle(fontSize: 20)),
+                                Text(
+                                  vehicle.data.name,
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                                Spacer(),
+                                if (notAuthenticatedDevices.contains(
+                                    vehicle.device.remoteId.toString()))
+                                  Icon(
+                                    Icons.pin,
+                                    color: Colors.amber,
+                                  ),
                               ],
                             ),
                             Padding(
                               padding:
                                   const EdgeInsets.only(left: 8.0, bottom: 4.0),
                               child: Text(
-                                  "Mac Address: ${vehicleEntry.vehicleData.macAddress}, Association ID: ${vehicleEntry.vehicleData.associationId}",
+                                  "Mac Address: ${vehicle.data.macAddress}, Association ID: ${null}",
                                   style: const TextStyle(fontSize: 10)),
                             ),
                             Row(
                               mainAxisSize: MainAxisSize.min,
                               children: [
                                 IconButton(
-                                  icon: Icon(vehicleEntry.doorsLocked
+                                  icon: Icon(vehicle.doorsLocked
                                       ? Icons.lock_outline
                                       : Icons.lock_open_outlined),
-                                  onPressed: vehicleEntry.isConnected
-                                      ? () {
-                                          setState(() {
-                                            BleService.postEvent(
-                                                vehicleEntry.doorsLocked
-                                                    ? "SEND_MESSAGE:ud\n"
-                                                    : "SEND_MESSAGE:ld\n");
-                                          });
+                                  onPressed: vehicle.device.isConnected
+                                      ? () async {
+                                          await BleService.sendMessage(
+                                            vehicle.device,
+                                            vehicle.doorsLocked ? "ud" : "ld",
+                                          );
                                         }
                                       : null,
                                 ),
-                                vehicleEntry.vehicleData.hasTrunkUnlock
+                                vehicle.data.hasTrunkUnlock
                                     ? IconButton(
                                         icon: const Icon(Icons.directions_car),
-                                        onPressed: vehicleEntry.isConnected
-                                            ? () {
-                                                BleService.postEvent(
-                                                    "SEND_MESSAGE:ut\n");
+                                        onPressed: vehicle.device.isConnected
+                                            ? () async {
+                                                await BleService.sendMessage(
+                                                  vehicle.device,
+                                                  "ut",
+                                                );
                                               }
                                             : null,
                                       )
                                     : Container(),
-                                vehicleEntry.vehicleData.hasEngineStart
+                                vehicle.data.hasEngineStart
                                     ? IconButton(
                                         icon: const Icon(Icons.restart_alt),
-                                        onPressed: vehicleEntry.isConnected
+                                        onPressed: vehicle.device.isConnected
                                             ? () {
                                                 BleService.postEvent(
                                                     "SEND_MESSAGE:st\n");
@@ -239,18 +282,43 @@ class _HomePageState extends State<HomePage> {
                         trailing: IconButton(
                           icon: const Icon(Icons.delete),
                           onPressed: () async {
-                            // Delete vehicle from Hive box
+                            final confirmed = showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text("Delete Vehicle"),
+                                content: Text(
+                                    "Are you sure you want to delete ${vehicle.data.name}?"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(false),
+                                    child: const Text("Cancel"),
+                                  ),
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: const Text("Delete"),
+                                  ),
+                                ],
+                              ),
+                            );
+                            if (!(await confirmed)) return;
+
                             VehicleStorage.removeVehicle(
-                                vehicleEntry.vehicleData.macAddress);
+                                vehicle.data.macAddress);
 
-                            print(
-                                "Removing Association ID: ${vehicleEntry.vehicleData.associationId}");
-                            await BleService.disassociateBle(
-                                vehicleEntry.vehicleData.associationId,
-                                vehicleEntry.vehicleData.macAddress);
+                            setState(() => vehicles.removeAt(index));
 
-                            _getVehicles();
-                            setState(() {});
+                            if (vehicle.device.isConnected)
+                              await BleService.disconnectDevice(vehicle.device);
+
+                            // print(
+                            //     "Removing Association ID: ${vehicleEntry.vehicleData.associationId}");
+                            // await BleService.disassociateBle(
+                            //     vehicleEntry.vehicleData.associationId,
+                            //     vehicleEntry.vehicleData.macAddress);
+
+                            getVehicles();
                           },
                         ),
                       );
@@ -261,16 +329,4 @@ class _HomePageState extends State<HomePage> {
           ],
         ));
   }
-}
-
-class VehicleEntry {
-  Vehicle vehicleData;
-  bool isConnected;
-  bool doorsLocked;
-
-  VehicleEntry({
-    required this.vehicleData,
-    required this.isConnected,
-    required this.doorsLocked,
-  });
 }
