7974544f436c5700beeb50ab41207750aec75a51
Mauznemo <mauznemo@gmail.com>
2025-07-15 17:58:34 +0200

    feat(mobile app): nicer labelText position

--- MobileApp/lib/components/custom_text_form_field.dart
+++ MobileApp/lib/components/custom_text_form_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class CustomTextFormField extends StatelessWidget {
+class CustomTextFormField extends StatefulWidget {
   final TextEditingController controller;
   final String labelText;
   final VoidCallback? onEditingComplete;
@@ -22,19 +22,54 @@ class CustomTextFormField extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  late FocusNode _focusNode;
+  bool _isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        _isFocused = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: obscureText,
-      validator: validator,
-      keyboardType: keyboardType,
-      inputFormatters: inputFormatters,
-      onEditingComplete: onEditingComplete,
+    bool isLabelFloating = _isFocused || (widget.controller.text.isNotEmpty);
+
+    return TextField(
+      controller: widget.controller,
+      focusNode: _focusNode,
+      obscureText: widget.obscureText,
+      keyboardType: widget.keyboardType,
+      inputFormatters: widget.inputFormatters,
+      onEditingComplete: () {
+        if (widget.onEditingComplete != null) {
+          widget.onEditingComplete!();
+        }
+        _focusNode.unfocus(); // Unfocus the input field
+      },
       decoration: InputDecoration(
         filled: true,
         fillColor: Theme.of(context).colorScheme.secondaryContainer,
         contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
-        labelText: labelText,
+        label: Container(
+          padding: EdgeInsets.only(bottom: isLabelFloating ? 25 : 0),
+          child: Text(widget.labelText),
+        ),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(50),
           borderSide: BorderSide.none,
