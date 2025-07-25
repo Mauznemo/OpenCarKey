61f04dd07a8a1e23bb323b919c4ba44b9a107e9c
Mauznemo <mauznemo@gmail.com>
2025-07-25 12:57:36 +0200

    fix(lock controller): not working on low active relay

--- Firmware/LockController/src/main.cpp
+++ Firmware/LockController/src/main.cpp
@@ -9,29 +9,29 @@ const int trunkRelayPin1 = 27;
 
 void openTrunk()
 {
-  digitalWrite(trunkRelayPin1, HIGH);
-  delay(50);
   digitalWrite(trunkRelayPin1, LOW);
+  delay(50);
+  digitalWrite(trunkRelayPin1, HIGH);
   Serial.println("ut");
 }
 
 void unlock()
 {
-  digitalWrite(doorsRelayPin1, HIGH);
-  digitalWrite(doorsRelayPin2, LOW);
-  delay(50);
   digitalWrite(doorsRelayPin1, LOW);
-  digitalWrite(doorsRelayPin2, LOW);
+  digitalWrite(doorsRelayPin2, HIGH);
+  delay(50);
+  digitalWrite(doorsRelayPin1, HIGH);
+  digitalWrite(doorsRelayPin2, HIGH);
   Serial.println("ud");
 }
 
 void lock()
 {
-  digitalWrite(doorsRelayPin1, LOW);
-  digitalWrite(doorsRelayPin2, HIGH);
-  delay(50);
-  digitalWrite(doorsRelayPin1, LOW);
+  digitalWrite(doorsRelayPin1, HIGH);
   digitalWrite(doorsRelayPin2, LOW);
+  delay(50);
+  digitalWrite(doorsRelayPin1, HIGH);
+  digitalWrite(doorsRelayPin2, HIGH);
   Serial.println("ld");
 }
 
@@ -64,9 +64,9 @@ void setup()
   pinMode(doorsRelayPin2, OUTPUT);
   pinMode(trunkRelayPin1, OUTPUT);
 
-  digitalWrite(doorsRelayPin1, LOW);
-  digitalWrite(doorsRelayPin2, LOW);
-  digitalWrite(trunkRelayPin1, LOW);
+  digitalWrite(doorsRelayPin1, HIGH); // You might need to swap high and low depending on if your relays are active low
+  digitalWrite(doorsRelayPin2, HIGH);
+  digitalWrite(trunkRelayPin1, HIGH);
 
   Serial.begin(115200);
 
